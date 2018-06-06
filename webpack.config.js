802e153bd71746b4789ebeb2de4274632b57d411
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2018-06-06 14:42:19 +0300

    new

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
-const path = require('path');
-var webpack = require('webpack');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path                 = require('path');
+var   webpack              = require('webpack');
+var   UglifyJSPlugin       = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin    = require("extract-text-webpack-plugin");
+var   MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     'polyfills': './src/polyfills.ts',
     'app' :      './src/main.ts'
   },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  performance: {
+    hints: false
   },
-  devServer: {
-    historyApiFallback: true,
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: '[name].js'
   },
    resolve: {
     extensions: ['.ts', '.js']
@@ -23,10 +24,7 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+        loaders: ["style-loader", "css-loader", "sass-loader"]
       },    
       {
         test: /\.ts$/, // определяем тип файлов
@@ -34,26 +32,20 @@ module.exports = {
         {
              loader: 'awesome-typescript-loader',
              options: { configFileName: path.resolve(__dirname, 'tsconfig.json') }
-           } ,
-            'angular2-template-loader'
+           } ,'angular2-template-loader'
         ]
-        }, 
-        {
-           test: /\.html$/,
-           loader: 'html-loader'
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-          loader: 'file-loader?name=assets/[name].[hash].[ext]'}
+      }
     ]
   },
   plugins: [
-    new ExtractTextPlugin('style.css'),
-    new webpack.ContextReplacementPlugin(
-      /angular(\\|\/)core/,
+  new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+  new webpack.ContextReplacementPlugin(
+      /angular(\|\/)core/,
       path.resolve(__dirname, 'src'), // каталог с исходными файлами
     {} // карта маршрутов
-    ),
-    new UglifyJSPlugin()
+  ),
+  new UglifyJSPlugin()
   ]
 };
