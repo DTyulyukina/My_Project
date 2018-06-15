f2e8451a0ba982bb9fa81390710b82fcbf105756
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2018-06-15 16:57:47 +0300

    new html

--- src/app/Calendar.component.ts
+++ src/app/Calendar.component.ts
@@ -3,10 +3,16 @@ import { CalendarEvent } from 'angular-calendar';
      
 @Component({ 
   selector: 'mwl-calendar-component',
-
+  template: `<mwl-calendar
+  [view] = "calendarView"
+  [view-date] = "viewDate"
+  [events] = "events"
+  [view-title] = "calendarTitle"
+  [on-event-click] = "eventClicked(calendarEvent)"
+  [on-event-times-changed] = "calendarEvent.startsAt = calendarNewEventStart; calendarEvent.endsAt = calendarNewEventEnd"
+  [cell-is-open] = "true">
+</mwl-calendar>`
 })
+
 export class Calendar {
-  view:     string = 'day';
-  viewDate: Date   = new Date();
-  events:   CalendarEvent[] = [];
 }
