f495a0f957984f72d04c455d6c963be0708ff37a
TEAM\Tyulyukina <Tyulyukina@competentum.ru>
2018-10-19 16:53:49 +0300

    add

--- src/app/Settings.component.ts
+++ src/app/Settings.component.ts
@@ -5,6 +5,16 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None, 
   template: `<div class="content">
+            <p>Цветовое оформление</p>
+            <input type="radio" checked="checked" />Дизайн 1
+            <input type="radio" checked="checked" />Дизайн 2
+            <input type="radio" checked="checked" />Дизайн 3
+            <p>Тектовное оформление</p>
+            <ul>
+            <li>Стиль</li>
+            <li>Размер</li>
+            <li>Форматирование</li>
+            </ul>
             </div>`
 })
 
